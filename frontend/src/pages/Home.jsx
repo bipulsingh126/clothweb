18f55f3f018f5e8400e2e8202eb47d7b5e4268a9
bipulsingh126 <bipulpratapsingh126@gmail.com>
2024-10-02 19:36:48 +0530

    "Updated App.jsx, Hero.jsx, Navbar.jsx, index.css, main.jsx, and Home.jsx with new components, routes, and styles."

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Hero from '../components/Hero.jsx'
+import LetestCollection from '../components/LetestCollection.jsx'
+import BestSeller from '../components/BestSeller.jsx'
+import OurPolicy from '../components/OurPolicy.jsx'
+import NewsLattter from '../components/NewsLattter.jsx'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <LetestCollection/>
+      <BestSeller/>
+      <OurPolicy/>
+      <NewsLattter/>
     </div>
   )
 }
