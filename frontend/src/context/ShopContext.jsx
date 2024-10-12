b4049c74f6a9b470787d738ac5b66b004de919d2
bipulsingh126 <bipulpratapsingh126@gmail.com>
2024-10-12 12:18:18 +0530

    "Updated ShopContext to add cart items state and addTocart function, and modified Product page to include size selection, add to cart button, and related product display"

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -5,16 +5,31 @@ export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
 
-const currency = '₹';
-const delivery_fee = 40; 
+    const currency = '₹';
+    const delivery_fee = 40;
 
-const [search , setSearch] = useState('');
-const [showSearch , setShowSearch] = useState(false);
+    const [search, setSearch] = useState('');
+    const [showSearch, setShowSearch] = useState(false);
+    const [cartItems, setCartItems] = useState({});
+
+
+
+
+    const addTocart = async (itemId, size) => {
+        let cartData = structuredClone(cartItems);
+        if (cartData[itemId]) {
+            if(cartData[itemId][size]){
+                
+            }
+        
+    }
+
+    }
 
 
     const value = {
-        products, currency , delivery_fee,
-        search , setSearch , showSearch, setShowSearch
+        products, currency, delivery_fee,
+        search, setSearch, showSearch, setShowSearch
     }
 
     return (
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/frontend_assets/assets';
+import RelatedProduct from '../components/RelatedProduct';
 
 const Product = () => {
 
@@ -9,6 +10,7 @@ const Product = () => {
   const { products, currency } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState('');
+  const [size, setSize] = useState();
 
   const fetchProductData = async () => {
     products.map((item) => {
@@ -57,11 +59,36 @@ const Product = () => {
           <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description} </p>
           <div className='flex flex-col gap-4 my-8 '>
             <p>Select Size </p>
-
+            <div className='flex gap-2'>
+              {productData.sizes.map((item, index) => (
+                <button onClick={() => setSize(item)} className={`border py-2  px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''} `} key={index}>{item} </button>
+              ))}
+            </div>
+          </div>
+          <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART </button>
+          <hr className='mt-8 sm:w-4/5' />
+          <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+            <p>100% Original Product. </p>
+            <p>Cash on delivery is avilable on this product. </p>
+            <p>Easy  return and exchange policy within 7 days. </p>
           </div>
         </div>
       </div>
-
+      {/* description & review section */}
+      <div className='mt-20'>
+        <div className='flex'>
+          <b className='border px-5 py-3 text-sm'>
+            Description
+          </b>
+          <p className='border px-5 py-3 text-sm'>Rreview (122) </p>
+        </div>
+        <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+          <p>From casual everyday wear to elegant evening attire, our range covers all occasions. Discover trendy tops, versatile bottoms, chic dresses, and comfortable loungewear. We prioritize comfort and durability. Each piece is crafted from high-quality fabrics that feel great against your skin and stand the test of time.</p>
+          <p>We cater to all body types with an inclusive size range, ensuring that everyone can find something fabulous to wear. Complete your look with our selection of stylish accessories, including bags, jewelry, and footwear that add the perfect finishing touch. </p>
+        </div>
+      </div>
+      {/* display reltaed product */}
+      <RelatedProduct category={productData.category} subCategory={productData.subCategory} />
     </div>
   ) : <div className='opacity-0'> </div>
 }
