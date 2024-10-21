5d1e60f9c1a4c2aeda31906ca2d64a6a12486e08
bipulsingh126 <bipulpratapsingh126@gmail.com>
2024-10-21 23:52:38 +0530

    "Updated admin login token generation, added admin authentication middleware to product routes, and configured Vite server port"

--- Backend/controllers/usercontroller.js
+++ Backend/controllers/usercontroller.js
@@ -108,7 +108,7 @@ const adminLogin = async (req, res) => {
     try {
         const { email, password } = req.body
         if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
-            const token = jwt.sign(email, password, process.env.JWT_SECRET);
+            const token = jwt.sign(email+password, process.env.JWT_SECRET);
             res.status(200).json({
                 success: true,
                 token
