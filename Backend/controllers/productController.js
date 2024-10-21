234f4c3000082d893555906a54b7089c419d6254
bipulsingh126 <bipulpratapsingh126@gmail.com>
2024-10-21 22:23:00 +0530

    "Formatted code in productController.js and usercontroller.js files, added whitespace and corrected indentation."

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,75 +1,118 @@
-import {v2 as cloudnary} from 'cloudinary';
+import { v2 as cloudnary } from 'cloudinary';
 import { productModel } from '../models/productModels.js';
 
 //add product controller
 
-const addProduct = async (req , res) => {
- try {
-    const {name , description , price , category, subCategory, sizes, bestseller} = req.body;
-    const image1 = req.files.image1 && req.files.image1[0]
-    const image2 = req.files.image2 && req.files.image2[0]
-    const image3 = req.files.image3 && req.files.image3[0]
-    const image4 = req.files.image4 && req.files.image4[0]
+const addProduct = async (req, res) => {
+   try {
+      const { name, description, price, category, subCategory, sizes, bestseller } = req.body;
+      const image1 = req.files.image1 && req.files.image1[0]
+      const image2 = req.files.image2 && req.files.image2[0]
+      const image3 = req.files.image3 && req.files.image3[0]
+      const image4 = req.files.image4 && req.files.image4[0]
 
-    const images = [image1, image2, image3, image4].filter((item)=> item !== undefined)
-     let imagesUrl = await Promise.all(
-        images.map(async (item)=>{
-            let result = await cloudnary.uploader.upload(item.path, {resource_type: 'image'});
+      const images = [image1, image2, image3, image4].filter((item) => item !== undefined)
+      let imagesUrl = await Promise.all(
+         images.map(async (item) => {
+            let result = await cloudnary.uploader.upload(item.path, { resource_type: 'image' });
             return result.secure_url
-        })
-     )
-     
-     const productData =  {
-        name,
-        description,
-        price: Number(price),
-        category,
-        subCategory,
-        sizes: JSON.parse(sizes) ,
-        bestseller: bestseller === 'true' ? true : false,
-        image: imagesUrl,
-        date : Date.now()
-     }
-const product = new productModel(productData);
-await product.save();
+         })
+      )
 
-res.status(200).json({
-    success: true,
-    message: 'Product added successfully',
-    data: product,
-})
+      const productData = {
+         name,
+         description,
+         price: Number(price),
+         category,
+         subCategory,
+         sizes: JSON.parse(sizes),
+         bestseller: bestseller === 'true' ? true : false,
+         image: imagesUrl,
+         date: Date.now()
+      }
+      const product = new productModel(productData);
+      await product.save();
 
- } catch (error) {
-    console.log(error);
-    res.status(500).json({
-        success: false,
-        message: error.message,
-    })
-    
- }
+      res.status(200).json({
+         success: true,
+         message: 'Product added successfully',
+         data: product,
+      })
+
+   } catch (error) {
+      console.log(error);
+      res.status(500).json({
+         success: false,
+         message: error.message,
+      })
+
+   }
 }
 
 
 //list products
 
-const listProducts = async (req , res) => {
+const listProducts = async (req, res) => {
+   try {
+      const products = await productModel.find({});
+      res.status(200).json({
+         success: true,
+         data: products,
+      })
+   } catch (error) {
+      console.log(error);
+      res.status(500).json({
+         success: false,
+         message: error.message,
+      })
 
+   }
 }
 
 
 // removeing product 
 
-const removeProduct = async (req , res) => {
-
+const removeProduct = async (req, res) => {
+   try {
+      await productModel.findByIdAndDelete(req.body.id);
+      res.status(200).json({
+         success: true,
+         message: 'Product removed successfully',
+      })
+   } catch (error) {
+      console.log(error);
+      res.status(500).json({
+         success: false,
+         message: error.message,
+      })
+   }
 }
 
 
 
 //single product
 
-const singleProduct = async (req , res) => {
+const singleProduct = async (req, res) => {
+   try {
+      const { productId } = req.body;
+      const product = await productModel.findById(productId);
+      res.status(200).json({
+         success: true,
+         message: "product find success",
+         product
+      })
+   } catch (error) {
+      console.log(error);
+      res.status(500).json({
+         success: false,
+         message: error.message,
+      })
 
+   }
 }
 
 
-export {removeProduct, singleProduct, listProducts , addProduct};
+
+
+
+export { removeProduct, singleProduct, listProducts, addProduct };
--- Backend/controllers/usercontroller.js
+++ Backend/controllers/usercontroller.js
@@ -4,113 +4,126 @@ import bcrypt from "bcrypt";
 import jwt from 'jsonwebtoken';
 
 
-const createToken  = (id) =>{
-    return  jwt.sign({id},process.env.JWT_SECRET)
+const createToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET)
 }
 
 //loging controller
-const loginUser = async (req ,res )=>{
-try {
-    const {email , password} = req.body;
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
 
-    const user = await usermodel.findOne({email});
+        const user = await usermodel.findOne({ email });
 
-    if (!user) {
-        return res.status(404).json({
-            success : false,
-            message : "User not found"
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch) {
+            const token = createToken(user._id);
+            return res.status(200).json({
+                success: true,
+                message: "Logged in successfully",
+                token
+            })
+        } else {
+            return res.status(500).json({
+                success: false,
+                message: "Invalid password"
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: error.message
         })
+
+
     }
-    
-    const isMatch =  await bcrypt.compare(password, user.password);
-    if (isMatch) {
-        const token = createToken(user._id);
-        return res.status(200).json({
-            success : true,
-            message : "Logged in successfully",
-            token
-        })
-    }else{
-        return res.status(500).json({
-            success : false,
-            message : "Invalid password"
-        })
-    }
-} catch (error) {
-    console.log(error);
-    res.status(500).json({
-        success : false,
-        message : error.message
-    })
-
-    
-}
 }
 
 //user registertion 
 
-const registerUser = async(req , res )=>{
- try {
-    const {name ,email , password} =req.body;
-    // checking user exists
-    const exists = await usermodel.findOne({email});
-    if (exists) {
-     return res.status(200).json({
-        success : true,
-        message : "User already exists"
-     })
-    }
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        // checking user exists
+        const exists = await usermodel.findOne({ email });
+        if (exists) {
+            return res.status(200).json({
+                success: true,
+                message: "User already exists"
+            })
+        }
 
-    // validate email nd strong  password
-    if (!validator.isEmail(email)) {
-        return res.status(400).json({
-            success : false,
-            message : "Invalid email"
+        // validate email nd strong  password
+        if (!validator.isEmail(email)) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid email"
+            })
+        }
+        if (password.length < 8) {
+            return res.status(400).json({
+                success: false,
+                message: "Password should be at least 8 characters long"
+            })
+        }
+
+        // hashing user password
+        const salt = await bcrypt.genSalt(10)
+        const hashPassword = await bcrypt.hash(password, salt)
+
+        const newUser = new usermodel({
+            name,
+            email,
+            password: hashPassword
         })
-    }
-    if (password.length < 8) {
-        return res.status(400).json({
-            success : false,
-            message : "Password should be at least 8 characters long"
+
+        const user = await newUser.save();
+
+        const token = createToken(user._id)
+        res.status(200).json({
+            success: true,
+            message: "User registered successfully",
+            token
         })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+
     }
-    
-    // hashing user password
-    const salt  = await bcrypt.genSalt(10)
-    const hashPassword = await bcrypt.hash(password, salt)
-
-    const newUser = new usermodel({
-        name,
-        email,
-        password :  hashPassword
-    })
-
-    const user = await newUser.save();
-
-    const token = createToken(user._id)
-    res.status(200).json({
-        success : true,
-        message : "User registered successfully",
-        token
-    })
- } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-       success : false,
-       message : error.message
-    })
-    
- }
 }
 
 //admin login
-const adminLogin = async ( req , res )=>{
-try {
-    
-} catch (error) {
-    
-}
+const adminLogin = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            const token = jwt.sign(email, password, process.env.JWT_SECRET);
+            res.status(200).json({
+                success: true,
+                token
+            })
+        } else {
+            res.status(500).json({
+                success: false,
+                message: "Invalid credentials"
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
 
 }
 
-export {loginUser, registerUser,adminLogin}
+export { loginUser, registerUser, adminLogin }
